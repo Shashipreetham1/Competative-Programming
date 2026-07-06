26850714412c4f3dcc4b211122ce601b4660d2f9
Shashipreetham Miryala <145383320+Shashipreetham1@users.noreply.github.com>
2026-07-06 14:50:17 +0530

    Time: 3 ms (42.52%), Space: 15 MB (82.12%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
-    int removeCoveredIntervals(vector<vector<int>>& A) {
-        ranges::sort(A, {}, [](auto& x) {
-            return pair{x[0], -x[1]};
-        });
-        int res = 0, r = 0;
-        for (auto& x : A) {
-            res += x[1] > r;
-            r = max(r, x[1]);
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(),
+             [](vector<int>& a, vector<int>& b) {
+                 if (a[0] == b[0])
+                     return a[1] > b[1];
+                 return a[0] < b[0];
+             });
+        int count = 0;
+        int maxEnd = 0;
+        for (auto &it : intervals) {
+            if (it[1] > maxEnd) {
+                count++;
+                maxEnd = it[1];
+            }
         }
-        return res;
+        return count;
     }
 };
